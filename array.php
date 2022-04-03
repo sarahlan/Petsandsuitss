f30708bf9178c015a173975370206a24722d0654
Sarah Lancaster <Sarah.Lancaster@students.jmcss.org>
2022-04-03 14:26:01 -0500

    jowf

---
+++ array.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+    //string
+    $name = 'coding is fun';
+
+    //integer
+    $name = 20;
+
+     //float
+    $name = 20.5784;
+
+    //boolean
+
+
+    //array
+    $names = array("Sarah","Cody","Hadley","Ashlyn","Cade");
+    echo $names ['0']
+
+
+?>
+</body>
+</html>
