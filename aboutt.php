8096240a284fa59779e1fdd03fe165944f8ba83f
Guadalupe Vazquez <guadalupe.vazquez@students.jmcss.org>
2022-04-25 20:25:49 -0500

    Update aboutt.php

--- aboutt.php
+++ aboutt.php
@@ -13,9 +13,48 @@
     <link href="https://fonts.googleapis.com/css2?family=Reenie+Beanie&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/7b6acd16b3.js" crossorigin="anonymous"></script>
     <title>Documents</title>
+  
   </head>
 
-  <body>
+  <body class="about">
+    <!----------Navbar----------->
+    <nav class="navbar navbar-expand-lg" style="background-color:#E8EDC3;">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="index.php">About</a>
+              </li>
+    </nav>
+    <header class="page-header gradient">
+        <div class="container pt-3">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-7">
+              <h2 class="text-center wave">How did Pets and Suits start?</h2>
+            </div>
+          </div>
+        </div>
+        </path>
+        </svg>
+      </header>
+
+      
+      <div class="row">
+        <div class="col-md-2 side-piece"></div>
+        <div class="col-md-8 text-center">
+          <p1>
+            Pets and Suits started of as a way to have a friend. We originally had a friend that wanted to be able to communicate with her dog. 
+            She lived by herself and just wanted a buddy. For her birthday we created a suit for her dog to be able to interact with her and she loved it! 
+            Later on she also mentioned that she  wanted to be able to talk to her dog so we created a translator device so she would be able to communicate with him. 
+            Our translator device translates what your animal says into english. The device looks like a little boom box in the suit. That is how Pets and Suits was born. 
+            With our suits your pet can now interact with you as a friend would. Our suits are designed to a size of 5ft tall. It also has all four limbs and can assist your dog with anything. 
+            You can talk to them and play with them! With our suits there is a built in pet translator that can help you communicate with your pet. It can be adjusted to any language you like. 
+            We have so many different options of suits for dogs, cat, fish, monkey and bunny. 
+          </p1>
+        </div>
+        <div class="col-md-2 side-piece"></div>
+      </div>
+    </div>
 
   <img src="..." class="rounded float-start" alt="...">
   <div class="card">
